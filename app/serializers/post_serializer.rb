9be47fd5ed38a63a50718cc3c0baad6bfd65ed66
Joshua Welsh <j22yosh@gmail.com>
2022-09-11 15:56:10 -0400

    completed assignment

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,13 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :title, :content, :short_content
+
+  belongs_to :author
+ 
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+
+  def short_content
+    "#{self.object.content[0..39]}..."
+  end
+
+end
